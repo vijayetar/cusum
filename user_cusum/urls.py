f5e8a508c184c2e3180d675fb8859d8800444d6e
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-06-17 15:45:48 -0700

    details page completed

--- user_cusum/urls.py
+++ user_cusum/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import EventListView
+from .views import EventListView, EventDetailView
 
 urlpatterns = [
-  path("", EventListView.as_view(), name = 'event_list')
+  path("", EventListView.as_view(), name = 'event_list'),
+  path("event_detail/<int:pk>", EventDetailView.as_view(), name = 'event_detail')
 ]
--- user_cusum/views.py
+++ user_cusum/views.py
@@ -8,9 +8,9 @@ class EventListView(ListView):
   template_name = 'event_list.html'
   model = Event
 
-# class EventDetailView(DetailView):
-#   template_name = 'event_detail.html'
-#   model = Event
+class EventDetailView(DetailView):
+  template_name = 'event_detail.html'
+  model = Event
 
 # class EventCreateView(CreateView):
 #   template_name = 'event_create.html'
